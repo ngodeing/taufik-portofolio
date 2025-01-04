2fdcb0b853835f89ecde89fc6a844612d577aab6
ngodeing <onlytaufik47@gmail.com>
2025-01-04 16:05:12 +0700

    add redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <div>
-      hwllo
-    </div>
-  );
+export default function Page() {
+  redirect("/home");
+  return null;
 }
