a77733f57ee6f45b7c9c7b35b0fa3aeb686f8018
ngodeing <onlytaufik47@gmail.com>
2025-01-15 16:07:58 +0700

    add desc

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <section id="home" className="text-gray-300 text-center flex lg:flex-row flex-col lg:justify-between gap-20 lg:mt-0 lg:gap-0 justify-start items-center lg:w-[900px] xl:w-[1100px] w-full mx-auto lg:h-[88vh]">
       <div className="container lg:w-[500px] w-full px-8 lg:p-0">
         <h1 className="lg:text-6xl text-4xl font-bold text-start">Hi, I'm Taufik Nurrohman</h1>
-        <p className="my-6 lg:text-xl text-md text-start">I am a person with strong skills and interests in web and mobile development. I have the ability to adapt to the environment and am used to working collaboratively in a team to achieve common goals. <br></br><br></br>I am a responsible person and always ready to face new challenges. With a high spirit of learning, I am committed to giving the best contribution in every project that is worked on.</p>
+        <p className="my-6 lg:text-xl text-md text-start">I am a person with strong skills and interests in web and mobile development. I am a responsible person and always ready to face new challenges. With a high spirit of learning, I am committed to giving the best contribution in every project that is worked on.</p>
         <div className="flex justify-start lg:gap-4 gap-3 mt-10">
           <NextButton href="project">Projects</NextButton>
           <NextButton href="experience">Experience</NextButton>
