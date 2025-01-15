da44254a9fb04d035e1bc4e450f4ff3fab6aadfc
ngodeing <onlytaufik47@gmail.com>
2025-01-15 13:17:17 +0700

    add detail projects

---
+++ src/app/project/[id]/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import Image from "next/image";
+import data from "../../data/projects.json";
+import NextButton from "../../components/NextButton";
+
+const ProjectDetail = () => {
+  const { id } = useParams();
+  const projectId = id as string;
+
+  const project = data.find((item) => item.id === parseInt(projectId));
+
+  if (!project) {
+    return <h1 className="text-center text-2xl font-bold">Project Not Found</h1>;
+  }
+
+  return (
+    <div className="max-w-4xl mx-auto p-6 space-y-6">
+      <h1 className="text-2xl md:text-4xl font-bold text-center">{project.title}</h1>
+
+      <div className="flex justify-center">
+        <Image
+          src={project.image}
+          alt={project.title}
+          width={400}
+          height={400}
+          className="rounded shadow-md"
+        />
+      </div>
+
+      <p className="text-lg text-gray-500 rounded">{project.description}</p>
+
+      <div>
+        <h2 className="text-2xl font-semibold">Technologies Used:</h2>
+        <ul className="list-disc list-inside text-gray-700">
+          {project.techstack?.map((tech, index) => (
+            <li key={index} className="text-gray-500 p-2 rounded">
+              {tech}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h2 className="text-2xl font-semibold mb-4">Media Gallery:</h2>
+        {project.media?.map((media, index) => (
+          <div
+            key={index}
+            className={`flex flex-col md:flex-row mb-12 ${
+              index % 2 !== 0 && "md:flex-row-reverse"
+            }`}
+          >
+            <Image
+              src={media}
+              alt={project.media_title[index]}
+              width={400}
+              height={400}
+              className="rounded shadow-md"
+            />
+            <div
+              className={`mt-4 md:mt-0 md:ml-4 ${
+                index % 2 !== 0 ? "md:text-right md:mr-4" : "md:text-left"
+              }`}
+            >
+              <h3 className="text-xl font-semibold">{project.media_title[index]}</h3>
+              <p className="text-gray-500 py-2 rounded">
+                {project.media_desc[index]}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="text-center py-10">
+        <NextButton href={project.link}>Visit the Project</NextButton>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetail;
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,4 +1,5 @@
 import projects from '../data/projects.json';
+import Link from 'next/link';
 
 export default function Projects() {
   return (
@@ -6,7 +7,7 @@ export default function Projects() {
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-3 lg:px-5">
           {projects.map((project, index) => (
-            <a key={index} href={project.link} target="_blank" rel="noopener noreferrer" className="border border-gray-700 hover:bg-gray-950 lg:hover:scale-105 transition duration-200 rounded-lg overflow-hidden shadow-lg">
+            <Link key={index} href={`/project/${project.id}`} rel="noopener noreferrer" className="border border-gray-700 hover:bg-gray-950 lg:hover:scale-105 transition duration-200 rounded-lg overflow-hidden shadow-lg">
               <img src={project.image} alt={project.title} className="w-full h-48 object-cover"/>
               <div className="p-4">
                 <h3 className="text-xl font-bold">{project.title}</h3>
@@ -16,7 +17,7 @@ export default function Projects() {
                   <p className="mt-2 text-gray-400 text-sm">Until: {new Date(project.endDate).toLocaleDateString()}</p>
                 </div>
               </div>
-            </a>
+            </Link>
           ))}
         </div>
       </div>
